5039bdb4a85d25f70d32da187798138d0fa7c727
Omer Osman <omer.m.o.hussain@gmail.com>
2023-12-07 19:28:35 +0000

    Attempt fix

--- things/forms.py
+++ things/forms.py
@@ -3,10 +3,19 @@ from django import forms
 from .models import Thing
 
 # Create your forms here.
-class ThingForm(forms.Form):
+class ThingForm(forms.ModelForm):
     class Meta:
         model = Thing
-        fields = ['name', 'description', 'quantity']
-        widgets = {'description':forms.Textarea(), 'quantity':forms.NumberInput(attrs={'min': 0, 'max': 100})}
+        fields = ['name', 'description', 'quantity']  # Exclude 'created_at'
+
+    # Customize form fields
+    description = forms.CharField(widget=forms.Textarea)
+    quantity = forms.IntegerField(widget=forms.NumberInput)
+
+    def clean_quantity(self):
+        quantity = self.cleaned_data['quantity']
+        if quantity < 0 or quantity > 100:
+            raise forms.ValidationError("Quantity must be between 0 and 100.")
+        return quantity
 
 
